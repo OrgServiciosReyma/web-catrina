5b7d7f6de4d4348569147ec20953480239a4bab3
jorgeepena <jorge.pena@werkn.mx>
2024-06-04 11:42:35 -0600

    Imagenes de Life

--- resources/views/life.blade.php
+++ resources/views/life.blade.php
@@ -99,14 +99,27 @@
         bottom: 0px;
         left: -10vw !important;
     }
+    
+    .btn-life-float{
+        line-height: 1.5 !important;
+        padding: 60px 30px !important;
+        text-align: center !important;
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        z-index: 10;
+    }
 
-    .intro-section{
-        position: relative;
+    footer{
+        position: relative !important;
+        z-index: 99 !important;
     }
 </style>
 @endpush
 
 @section('content')
+<a href="{{ asset('files/menu_lavida.pdf') }}" target="_blank" class="btn btn-primary btn-life-float px-4">Menú de Servicios</a>
+
 <section class="section section-experiences">
     <img class="life-background" src="{{ asset('img/life-background.png') }}" alt="">
 
@@ -125,17 +138,15 @@
             
 
             <div class="col-md-3 parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/experience-1.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Travesías que promuevan el reprogramar el cuerpo y la mente.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/center/lobby/lobby-4.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-5 offset-xl parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/experience-2.jpg') }}" alt="" width="100%" class="mb-3">
+                <img class="content-img" src="{{ asset('img/la_vida/center/lobby/lobby-3.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-3 offset-md-1 offset-big parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/experience-3.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>En La Vida Wellness Center podrás tener disfrutar de travesías para relajar, reconectar y energizar  con guía y respaldo de profesionales de la salud que impactarán de manera positiva en tus hábitos y estilo de vida para volver al origen y recuperar la conciencia que dia a dia vamos olvidando en el ajetreo de nuestros tiempos.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/center/lobby/lobby-5.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
 
@@ -144,23 +155,21 @@
             <img class="falling-letter letter-2 parallax" data-rellax-speed="-2" src="{{ asset('img/letter-2.png') }}" alt="">
 
             <div class="col-md-6 offset-big parallax" data-rellax-speed="4">
-                <img class="content-img" src="{{ asset('img/experience-4.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Recupera consciencia desde el centro y el sentido de LA VIDA</p>
+                <img class="content-img" src="{{ asset('img/la_vida/center/albercas/alberca-3.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-3 offset-md-3 parallax" data-rellax-speed="-2">
-                <img class="content-img" src="{{ asset('img/experience-5.jpg') }}" alt="" width="100%" class="mb-3">
+                <img class="content-img" src="{{ asset('img/la_vida/center/albercas/alberca-12.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
 
         <div class="row position-relative">
             <div class="col-md-8 offset-md-2 parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/experience-6.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Somos anfitriones de bienestar para sensibilizar.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/center/albercas/alberca-1.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-8 offset-md-1 parallax" data-rellax-speed="-4">
-                <img class="content-img" src="{{ asset('img/experience-7.png') }}" alt="" width="100%" class="mb-3">
+                <img class="content-img" src="{{ asset('img/la_vida/center/albercas/alberca-4.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <img class="gold-branch branch-2 parallax" data-rellax-speed="2" src="{{ asset('img/gold-branch-1.svg') }}" alt="">
@@ -171,13 +180,11 @@
             <img class="falling-letter letter-4 parallax" data-rellax-speed="1" src="{{ asset('img/letter-4.png') }}" alt="">
 
             <div class="col-md-6 offset-big parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/experience-4.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Generamos un verdadero cambio para relajar, reconectar y energizar.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/center/regaderas/regadera-1.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-6 parallax" data-rellax-speed="4">
-                <img class="content-img" src="{{ asset('img/experience-8.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>para alcanzar el máximo potencial emocional.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/center/albercas/alberca-11.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
     </div>
@@ -185,7 +192,7 @@
 
 <section id="travesias" class="section section-experiences">
     <div class="overlay"></div>
-    <img class="life-background" src="{{ asset('img/life-background.png') }}" alt="">
+    <img class="life-background" src="{{ asset('img/la_vida/travesias/hidroterapia/hidro-18.jpg') }}" alt="" style="opacity: .5">>
 
     <p class="special-welcome decorative">Día con día ponemos todo nuestro esfuerzo en hacer sentir a cada uno de nuestros invitados como en casa.</p>
 
@@ -198,21 +205,20 @@
 <section class="section intro-section">
     <div class="container">
         <div class="row position-relative">
+            {{--  
             <img class="gold-branch branch-1 parallax" data-rellax-speed="2" src="{{ asset('img/gold-branch-2.svg') }}" alt="">
-            
+            --}}
 
             <div class="col-md-3 parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/spa-1.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Queremos que hagan suya Hacienda Catrina, que disfruten de todos los detalles de auténtido lujo mexicano que elegimos en cada uno los espacios y momentos en lo que estarán con nosotros, diseñados y elegidos con la calidez, amabilidad y sentido de hospitalidad con orgullo y tradición que nos caracteriza a nuestra gente y a nuestro país.</p>
-            </div>
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/hidroterapia/hidro-13.jpg') }}" alt="" width="100%" class="mb-3">
+             </div>
 
             <div class="col-md-3 offset-md-3 offset-xl parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/gallery-spa-2.jpg') }}" alt="" width="100%" class="mb-3">
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/hidroterapia/hidro-5.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-3 offset-big parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/gallery-spa-3.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Recupera consciencia desde el centro y el sentido de LA VIDA</p>
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/hidroterapia/hidro-17.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
 
@@ -221,13 +227,27 @@
             <img class="falling-letter letter-4 parallax" data-rellax-speed="1" src="{{ asset('img/letter-4.png') }}" alt="">
 
             <div class="col-md-3 offset-big parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/spa-7.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Equipado y cómodo dentro del hotel.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/masajes/masaje-6.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-6 offset-md-3 parallax" data-rellax-speed="4">
-                <img class="content-img" src="{{ asset('img/spa-8.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Reserva para tus traslados más cómodos</p>
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/masajes/masaje-4.jpg') }}" alt="" width="100%" class="mb-3">
+            </div>
+        </div>
+
+        <div class="row position-relative">
+            <img class="gold-branch branch-1 parallax" data-rellax-speed="2" src="{{ asset('img/gold-branch-2.svg') }}" alt="">
+            
+            <div class="col-md-3 parallax" data-rellax-speed="1">
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/masaje/hidro-1.jpg') }}" alt="" width="100%" class="mb-3">
+             </div>
+
+            <div class="col-md-3 offset-md-3 offset-xl parallax" data-rellax-speed="2">
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/tunel/tunel-2.jpg') }}" alt="" width="100%" class="mb-3">
+            </div>
+
+            <div class="col-md-3 offset-big parallax" data-rellax-speed="2">
+                <img class="content-img" src="{{ asset('img/la_vida/travesias/tunel/tunel-4.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
     </div>
@@ -235,7 +255,7 @@
 
 <section id="gimnasio" class="section section-experiences">
     <div class="overlay"></div>
-    <img class="life-background" src="{{ asset('img/life-background.png') }}" alt="">
+    <img class="life-background" src="{{ asset('img/la_vida/gimnasio/gimnasio/gimnasio-1.jpg') }}" alt="" style="opacity: .5">
 
     <p class="special-welcome decorative">Día con día ponemos todo nuestro esfuerzo en hacer sentir a cada uno de nuestros invitados como en casa.</p>
 
@@ -251,19 +271,16 @@
         <div class="row position-relative">
             <img class="gold-branch branch-1 parallax" data-rellax-speed="2" src="{{ asset('img/gold-branch-2.svg') }}" alt="">
             
-
             <div class="col-md-3 parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/spa-1.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Queremos que hagan suya Hacienda Catrina, que disfruten de todos los detalles de auténtido lujo mexicano que elegimos en cada uno los espacios y momentos en lo que estarán con nosotros, diseñados y elegidos con la calidez, amabilidad y sentido de hospitalidad con orgullo y tradición que nos caracteriza a nuestra gente y a nuestro país.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/gimnasio/barberia/barberia-1.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-3 offset-md-3 offset-xl parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/gallery-spa-2.jpg') }}" alt="" width="100%" class="mb-3">
+                <img class="content-img" src="{{ asset('img/la_vida/gimnasio/barberia/barberia-2.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-3 offset-big parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/gallery-spa-3.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Recupera consciencia desde el centro y el sentido de LA VIDA</p>
+                <img class="content-img" src="{{ asset('img/la_vida/gimnasio/barberia/barberia-4.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
 
@@ -272,13 +289,11 @@
             <img class="falling-letter letter-4 parallax" data-rellax-speed="1" src="{{ asset('img/letter-4.png') }}" alt="">
 
             <div class="col-md-3 offset-big parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/spa-7.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Equipado y cómodo dentro del hotel.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/gimnasio/gimnasio/gimnasio-2.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-6 offset-md-3 parallax" data-rellax-speed="4">
-                <img class="content-img" src="{{ asset('img/spa-8.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Reserva para tus traslados más cómodos</p>
+                <img class="content-img" src="{{ asset('img/la_vida/gimnasio/gimnasio/gimnasio-3.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
     </div>
@@ -286,7 +301,7 @@
 
 <section id="temazcal" class="section section-experiences">
     <div class="overlay"></div>
-    <img class="life-background" src="{{ asset('img/life-background.png') }}" alt="">
+    <img class="life-background" src="{{ asset('img/la_vida/temazcal/temazcal-4.jpg') }}" alt="" style="opacity: .5">
 
     <p class="special-welcome decorative">Día con día ponemos todo nuestro esfuerzo en hacer sentir a cada uno de nuestros invitados como en casa.</p>
 
@@ -296,45 +311,40 @@
     </div>
 </section>
 
-
 <section class="section intro-section">
     <div class="container">
-        <div class="row position-relative">
+        <div class="row offset-big position-relative">
             <img class="gold-branch branch-1 parallax" data-rellax-speed="2" src="{{ asset('img/gold-branch-2.svg') }}" alt="">
             
-
             <div class="col-md-3 parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/spa-1.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Queremos que hagan suya Hacienda Catrina, que disfruten de todos los detalles de auténtido lujo mexicano que elegimos en cada uno los espacios y momentos en lo que estarán con nosotros, diseñados y elegidos con la calidez, amabilidad y sentido de hospitalidad con orgullo y tradición que nos caracteriza a nuestra gente y a nuestro país.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/temazcal/temazcal-1.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-3 offset-md-3 offset-xl parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/gallery-spa-2.jpg') }}" alt="" width="100%" class="mb-3">
+                <img class="content-img" src="{{ asset('img/la_vida/temazcal/temazcal-2.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-3 offset-big parallax" data-rellax-speed="2">
-                <img class="content-img" src="{{ asset('img/gallery-spa-3.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Recupera consciencia desde el centro y el sentido de LA VIDA</p>
+                <img class="content-img" src="{{ asset('img/la_vida/temazcal/temazcal-3.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
 
-        <div class="row offset-md position-relative">
+        <div class="row offset-big position-relative">
             <img class="falling-letter letter-3 parallax" data-rellax-speed="2" src="{{ asset('img/letter-3.png') }}" alt="">
             <img class="falling-letter letter-4 parallax" data-rellax-speed="1" src="{{ asset('img/letter-4.png') }}" alt="">
 
             <div class="col-md-3 offset-big parallax" data-rellax-speed="1">
-                <img class="content-img" src="{{ asset('img/spa-7.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Equipado y cómodo dentro del hotel.</p>
+                <img class="content-img" src="{{ asset('img/la_vida/temazcal/temazcal-5.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
 
             <div class="col-md-6 offset-md-3 parallax" data-rellax-speed="4">
-                <img class="content-img" src="{{ asset('img/spa-8.jpg') }}" alt="" width="100%" class="mb-3">
-                <p>Reserva para tus traslados más cómodos</p>
+                <img class="content-img" src="{{ asset('img/la_vida/temazcal/temazcal-6.jpg') }}" alt="" width="100%" class="mb-3">
             </div>
         </div>
     </div>
 </section>
 
+{{--  
 <section id="productos" class="section section-experiences">
     <div class="overlay"></div>
     <img class="life-background" src="{{ asset('img/life-background.png') }}" alt="">
@@ -385,6 +395,7 @@
         </div>
     </div>
 </section>
+--}}
 
 <div class="section white-scroll-text">
     @include('layouts.utilities.scroll_text')
